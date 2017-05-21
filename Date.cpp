18dedfd127a517c4c98bf172ff1d35a3a1073649
Andrew cabrera <acabrera15@saddleback.edu>
2017-05-20 17:04:19 -0700

    doxygen comments

--- Date.cpp
+++ Date.cpp
@@ -4,24 +4,48 @@
 
 #include "Date.h"
 
+/**
+ * @brief Date::Date
+ * initialized a date class to 1/1/1995
+ */
 Date::Date() {
     day = 01;
     month = 01;
     year = 1995;
 }
 
+/**
+ * @brief Date::Date
+ * @param m
+ * @param d
+ * @param y
+ * initializes A Date class with the input parameters.  The
+ * first sets day, second sets month, and third sets year
+ */
 Date::Date(int m, int d, int y) {
     day = d;
     month = m;
     year = y;
 }
 
+/**
+ * @brief Date::Date
+ * @param otherDate
+ * copy constructor for Date class
+ */
 Date::Date(const Date &otherDate) {
     day = otherDate.getDay();
     month = otherDate.getMonth();
     year = otherDate.getYear();
 }
 
+/**
+ * @brief Date::operator==
+ * @param otherDate
+ * @return
+ * returns true if day, month, and year are equal to that of the
+ * inputted Date class.  Returns false otherwise
+ */
 bool Date::operator==(const Date& otherDate) const {
     return (
         day == otherDate.getDay() &&
@@ -30,12 +54,26 @@ bool Date::operator==(const Date& otherDate) const {
     );
 }
 
+/**
+ * @brief Date::operator!=
+ * @param otherDate
+ * @return
+ * returns true if any of the Date instance variables are not
+ * equal to that of the Date class inputted to the function
+ */
 bool Date::operator!=(const Date &otherDate) const {
     return day != otherDate.getDay() ||
            month != otherDate.getMonth() ||
            year != otherDate.getYear();
 }
 
+/**
+ * @brief Date::operator>
+ * @param otherDate
+ * @return
+ * returns true if the current date is later than the Date inputted to the
+ * function.  It checks year, month, and day
+ */
 bool Date::operator>(const Date &otherDate) const {
     bool isGreater = false;
 
@@ -53,6 +91,15 @@ bool Date::operator>(const Date &otherDate) const {
     return isGreater;
 }
 
+/**
+ * @brief Date::operator>=
+ * @param otherDate
+ * @return
+ * returns true if the current Date object has greater than or equal to
+ * date than the date inputted to the class.  It calls the overloaded ==
+ * to check for class equality and class the overloaded > to check to see
+ * if the class is greater than the inputted class
+ */
 bool Date::operator>=(const Date &otherDate) const {
     bool isGreater = false;
 
@@ -64,6 +111,11 @@ bool Date::operator>=(const Date &otherDate) const {
     return isGreater;
 }
 
+/**
+ * 
+ * @param otherDate
+ * @return
+ */
 bool Date::operator<(const Date &otherDate) const {
     bool isLessThan = false;
 
